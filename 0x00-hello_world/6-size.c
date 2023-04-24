91570cd5bc8ab8ab93d5dc831b37107e12409b37
D3vinah <devinahmanyasi@gmail.com>
2023-04-24 13:39:05 -0700

    various sizez of computer types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of  various types on computer it is compiled
- * Return 0
+ * main - A program that prints the size of various types on computer it is compiled and run on
+ * Return 0 (Success)
  */
 int main(void)
 {
